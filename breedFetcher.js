2d061e7c13489873448aa101b05e14479fba3f83
Jodie Drover <jodiedrover@live.com>
2022-09-11 19:19:47 -0400

    Refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,10 +5,11 @@ const arg = process.argv.slice(2);
 request('https://api.thecatapi.com/v1/breeds/search?q=' + arg, (error, response, body) => {
   if (error) {
     console.log('error', error);
+    return;
   }
   const data = JSON.parse(body);
   if (data.length === 0) {
-    console.log("Breed cannot be found");
+    console.log("This breed cannot be found.");
   } else {
     console.log(data[0].description);
   }
