c698e0913ce4966dda5e1ba9077407c32e76736f
Jodie Drover <jodiedrover@live.com>
2022-09-11 21:07:32 -0400

    Refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,26 @@
 // breedFetcher.js
 const request = require('request');
-const arg = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + arg, (error, response, body) => {
-  if (error) {
-    console.log('error', error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("This breed cannot be found.");
-  } else {
-    console.log(data[0].description);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode}`), null);
+    }
+
+    const data = JSON.parse(body)
+    
+    if (data.length === 0) {
+      console.log("This breed cannot be found.");
+      return;
+    }
+    
+    const result = data[0].description
+
+    callback(null, result);
+  });
+};
+
+module.exports = { fetchBreedDescription };
