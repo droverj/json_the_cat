3b35e1702903b83cf4f3a64e69280b65ab1aa189
Jodie Drover <jodiedrover@live.com>
2022-09-12 14:59:20 -0400

    Correct breed not found message

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,23 +3,18 @@ const request = require('request');
 
 
 const fetchBreedDescription = function(breedName, callback) {
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
-    if (error) return callback(error, null);
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, _, body) => {
+  if (error) return callback(error, null);
 
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode}`), null);
-    }
+    const data = JSON.parse(body);
 
-    const data = JSON.parse(body)
-    
     if (data.length === 0) {
-      console.log("This breed cannot be found.");
-      return;
+     return callback(`Breed not found`, null);
     }
-    
-    const result = data[0].description
 
-    callback(null, result);
+    const result = data[0].description;
+
+    return callback(null, result);
   });
 };
 
